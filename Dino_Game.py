33d74b3624d422b80bb2aab9b9375440583a0ee3
Mrpnh <68578968+Mrpnh@users.noreply.github.com>
2020-12-04 22:41:36 +0530

    Final edit

--- Dino_Game.py
+++ Dino_Game.py
@@ -9,11 +9,12 @@ import random
 pygame.init()
 pygame.mixer.init()
 pygame.mixer.music.load("point.mp3")
+
 #Game variables
 
 screenW=1200
 screenH=300
-exit_game=False
+
 
 # Rendering Text
 
@@ -42,90 +43,122 @@ for i in range(2):
 for i in range(4):
     dino[i]=pygame.transform.scale(dino[i],(80,80))
 
-#Gaming variables
-bgfinal=0
-cactus_x=1300
-bird_x=1300
-bird_y=0     
-current_cactus=random.choice(cactus)
-score=0
-reset=0
-bird_index=0
-dino_index=0
-dino_y=160
-jumpCount=10
-isJump=False
-neg=1
-dino_x=90
-cactus_y=140
+
+
+# regame loop
+
+def regame(bgfinal,dino_x,dino_y,cactus_x,cactus_y,dino_index,current_cactus):
+    print("Yes")
+    restart=False
+    while not restart:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type==pygame.KEYDOWN:
+                    if event.key==pygame.K_SPACE:
+                        restart=True
+        gameWindow.blit(bg,(bgfinal,0))
+        gameWindow.blit(dino[dino_index%4],(dino_x,dino_y))
+        gameWindow.blit(current_cactus,(cactus_x,cactus_y))
+        pygame.display.update()
+        clock.tick(100)
+    gameloop()
+
 #gameloop
+def gameloop():
+    #Gaming variables
+    bgfinal=0
+    cactus_x=1300
+    bird_x=1300
+    bird_y=0     
+    current_cactus=random.choice(cactus)
+    score=0
+    reset=0
+    bird_index=0
+    dino_index=0
+    dino_y=160
+    jumpCount=10
+    isJump=False
+    neg=1
+    dino_x=90
+    cactus_y=140 
+    exit_game=False   
+    while not exit_game:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                exit_game=True
+                break
+            if event.type==pygame.KEYDOWN:
+                    if event.key==pygame.K_SPACE:
+                        isJump=True
+                        pygame.mixer.music.play()
 
-while not exit_game:
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            exit_game=True
-            break
-        if event.type==pygame.KEYDOWN:
-                if event.key==pygame.K_SPACE:
-                    isJump=True
-                    pygame.mixer.music.play()
-
-    # background running
-    
-    bgfinal=bgfinal%bg.get_width()
-    gameWindow.blit(bg,(bgfinal,0))
-    bgfinal-=7
-    gameWindow.blit(bg,(bgfinal-bg.get_width(),0))
-    
-    # cactus running
-    
-    gameWindow.blit(current_cactus,(cactus_x,cactus_y))
-    cactus_x-=7
-
-    if cactus_x<0:
-        current_cactus=random.choice(cactus)
-        cactus_x=1300
-
-    # Text on window 
-    textRender("Score:"+str(score),(0,0,0),20,30,32)
-    
-    reset+=1
-    if reset==100:
-        score+=1
-        reset=0
-    
-    bird_index+=1
-    bird_x-=7
-
-    if isJump:
-        if jumpCount >=-10:
-            neg=1
-            if jumpCount<0:
-                neg=-1
-            dino_y-= (jumpCount**2)*0.7*neg
-            jumpCount-=1
-            dino_x+=2
-        else:
-            isJump=False
-            jumpCount=10
-            dino_x=90
+        # background running
         
-    if dino_x==cactus_x-6:
-        print(dino_x,cactus_x)
-    
-    print(bird_x,bird_y)
-    if score>5:
-        gameWindow.blit(bird[bird_index%2],(bird_x,bird_y))
-    
-    gameWindow.blit(dino[dino_index%4],(dino_x,dino_y))
-    dino_index+=1
-   
-    if score%random.choice([10,20,100,40,80,50,60])==0:
-        if bird_x<0:
-            bird_x=1300
-            bird_y=random.choice([160,90])
-    
-    pygame.display.update()
-    clock.tick(100)
+        bgfinal=bgfinal%bg.get_width()
+        gameWindow.blit(bg,(bgfinal,0))
+        bgfinal-=7
+        gameWindow.blit(bg,(bgfinal-bg.get_width(),0))
+        
+        # cactus running
+        
+        gameWindow.blit(current_cactus,(cactus_x,cactus_y))
+        cactus_x-=7
 
-pygame.quit()
+        if cactus_x<0:
+            current_cactus=random.choice(cactus)
+            cactus_x=1300
+
+        # Text on window 
+        textRender("Score:"+str(score),(0,0,0),20,30,32)
+        
+        reset+=1
+        if reset==100:
+            score+=1
+            reset=0
+        
+        bird_index+=1
+        bird_x-=7
+
+        if isJump:
+            if jumpCount >=-10:
+                neg=1
+                if jumpCount<0:
+                    neg=-1
+                dino_y-= (jumpCount**2)*0.7*neg
+                jumpCount-=1
+                dino_x+=4
+            else:
+                isJump=False
+                jumpCount=10
+                dino_x=90
+            
+        if dino_x==cactus_x-6:
+            break
+        
+        if dino_y==bird_y and dino_x==bird_x-6:
+           break
+
+        if score>10:
+            gameWindow.blit(bird[bird_index%2],(bird_x,bird_y))
+        
+        gameWindow.blit(dino[dino_index%4],(dino_x,dino_y))
+        dino_index+=1
+    
+        if score%random.choice([10,20,100,40,80,50,60])==0:
+            if bird_x<0:
+                bird_x=1300
+                bird_y=random.choice([160,60])
+        
+        pygame.display.update()
+        clock.tick(100)
+
+    regame(bgfinal,dino_x,dino_y,cactus_x,cactus_y,dino_index,current_cactus)
+
+
+
+if __name__ == '__main__':
+    gameloop()
+    pygame.quit()
+    quit()
